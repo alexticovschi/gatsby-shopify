bdd5c2cf9cd4b2f638a878f1058f5d349d3b9f8e
Alex Ticovschi <alexmticovschi@gmail.com>
2020-12-11 18:15:12 +0000

    implementing routing for variants

--- src/templates/ProductTemplate/index.js
+++ src/templates/ProductTemplate/index.js
@@ -6,6 +6,7 @@ import { Layout, ImageGallery } from 'components';
 import { Grid, SelectWrapper, Price } from './styles';
 import CartContext from 'context/CartContext';
 import { navigate, useLocation } from '@reach/router';
+import queryString from 'query-string';
 
 export const query = graphql`
   query ProductQuery($shopifyId: String) {
@@ -31,18 +32,31 @@ const ProductTemplate = ({ data }) => {
   const { getProductById } = useContext(CartContext);
   const [product, setProduct] = useState(null);
   const [selectedVariant, setSelectedVariant] = useState(null);
+  const { origin, search, pathname } = useLocation();
+  // console.log(origin, pathname, search);
+  const variantId = queryString.parse(search).variant;
 
   useEffect(() => {
     const { shopifyId } = data.shopifyProduct;
 
     getProductById(shopifyId).then(result => {
       setProduct(result);
-      setSelectedVariant(result.variants[0]);
+      setSelectedVariant(
+        result.variants.find(({ id }) => id === variantId || result.variants[0])
+      );
+      console.log(variantId);
     });
-  }, [getProductById, setProduct, data.shopifyProduct]);
+  }, [getProductById, setProduct, data.shopifyProduct, variantId]);
 
   const handleVariantChange = e => {
-    setSelectedVariant(product?.variants.find(v => v.id === e.target.value));
+    const newVariant = product?.variants.find(v => v.id === e.target.value);
+    setSelectedVariant(newVariant);
+    navigate(
+      `${origin}${pathname}?variant=${encodeURIComponent(newVariant.id)}`,
+      {
+        replace: true,
+      }
+    );
   };
 
   return (
@@ -51,11 +65,11 @@ const ProductTemplate = ({ data }) => {
         <div>
           <h1>{data.shopifyProduct.title}</h1>
           <p>{data.shopifyProduct.description}</p>
-          {product?.availableForSale && (
+          {product?.availableForSale && !!selectedVariant && (
             <>
               <SelectWrapper>
                 <strong>Variant</strong>
-                <select onChange={handleVariantChange}>
+                <select value={variantId} onChange={handleVariantChange}>
                   {product?.variants.map(v => (
                     <option key={v.id} value={v.id}>
                       {v.title}

78cca2cbc327bf6d55ddcbdb667b555edbb98da9
Alex Ticovschi <alexmticovschi@gmail.com>
2020-12-15 09:35:29 +0000

    display the number of items and total price in header

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -5,12 +5,23 @@ import CartContext from 'context/CartContext';
 
 const Cart = () => {
   const { checkout } = useContext(CartContext);
+  console.log(checkout.lineItems);
 
-  console.log(checkout);
+  let totalQuantity = 0;
+
+  if (checkout) {
+    checkout.lineItems.forEach(
+      lineItem => (totalQuantity = totalQuantity + lineItem.quantity)
+    );
+  }
 
   return (
     <CartWrapper>
       <FaShoppingCart size="1.5em" />
+      <div>
+        {totalQuantity} {totalQuantity > 1 ? ' items' : 'item'} / £
+        {checkout?.totalPrice || '0.00'}
+      </div>
     </CartWrapper>
   );
 };
--- src/components/Cart/styles.js
+++ src/components/Cart/styles.js
@@ -2,4 +2,10 @@ import styled from 'styled-components';
 
 export const CartWrapper = styled.div`
   margin-left: auto;
+  display: flex;
+
+  div:last-child {
+    padding-left: 8px;
+    margin: auto 0;
+  }
 `;
